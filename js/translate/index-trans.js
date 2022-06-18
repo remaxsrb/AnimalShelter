eda5a6e70603c5526dd89598a96e245cd64e8564
Angell31 <82213604+Angell31@users.noreply.github.com>
2022-06-18 15:06:12 +0200

    pets.html
    Страница за све животиње и статички превод почетне странице

---
+++ js/translate/index-trans.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    let language = sessionStorage.getItem("language");
+    if(language==null){
+        language = "srb"
+        sessionStorage.setItem("language",language);
+    }
+})
